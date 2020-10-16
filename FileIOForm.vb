0ed5ece1f0049813cacecdcdfe873bcd18c1676c
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-16 08:53:49 -0600

    Add project files.

---
+++ FileIOForm.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+'Imports System.IO
+Public Class FileIOForm
+
+    Sub DontBeDumb() Handles Me.Load
+        Dim fileName As String = "C:\Users\rosstimo\Documents\__FileDemoStuff\myfile.txt"
+        AppendFile(fileName, "save me to a file...")
+
+
+
+        'FileOpen(1, "C:\Users\rosstimo\Documents\__FileDemoStuff\myfile.txt", OpenMode.Input)
+        'FileOpen(1, "C:\Users\rosstimo\Documents\__FileDemoStuff\myfile.txt", OpenMode.Output) 'overwrite
+        'FileOpen(1, "C:\Users\rosstimo\Documents\__FileDemoStuff\myfile.txt", OpenMode.Append) 'adds to file
+        'Write(1, "here is the new data")
+        'FileClose(1)
+
+        'FileOpen(1, "C:\Users\rosstimo\Documents\__FileDemoStuff\myfile.txt", OpenMode.Append) 'adds to file
+
+        'Me.Close()
+    End Sub
+
+    Private Sub AppendFile(ByVal filename As String, newRecord As String)
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filename, OpenMode.Append)
+            'Write(fileNumber, newRecord)
+            WriteLine(fileNumber, newRecord)
+
+        Catch ex As Exception
+            'TODO: user select file if it doesn't exist
+            'handle file in use exception
+            'verify other possible exceptions     
+            Console.WriteLine(ex.Message)
+        Finally
+            FileClose(fileNumber)
+        End Try
+
+    End Sub
+
+
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FileIODemo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FileIODemo")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a3c335d6-0ac5-4f7c-9a49-1c7c53c4b3c6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
